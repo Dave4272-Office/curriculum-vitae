d49cdd0d64ea5ef61da158448831ef6bab282c3f
Debraj Kundu <kundudebraj4272@gmail.com>
2021-08-09 21:27:42 +0530

    Added Navigation

---
+++ src/details/details.component.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./Details.sass";
+
+export function Details() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/edu">
+          <Edu />
+        </Route>
+        <Route path="/work">
+          <Work />
+        </Route>
+        <Route path="/certs">
+          <Certs />
+        </Route>
+        <Route path="/skills">
+          <Skills />
+        </Route>
+        <Route path="/interests">
+          <Intr />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Edu() {
+  return (
+    <div>
+      <h2>Education</h2>
+    </div>
+  );
+}
+
+function Work() {
+  return (
+    <div>
+      <h2>Work Experience</h2>
+    </div>
+  );
+}
+
+function Certs() {
+  return (
+    <div>
+      <h2>Certification</h2>
+    </div>
+  );
+}
+
+function Skills() {
+  return (
+    <div>
+      <h2>Skills</h2>
+    </div>
+  );
+}
+
+function Intr() {
+  return (
+    <div>
+      <h2>Interests</h2>
+    </div>
+  );
+}
