1f71a01d42c2c133bd0eb281a858cc97e15ecfa5
Debraj Kundu <kundudebraj4272@gmail.com>
2021-08-11 19:13:00 +0530

    Changed Look and Components.
    1) Styled Drawer & AppBar
    2) Separated individual components from content

---
+++ src/details/content/certs.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Certs extends React.Component {
+    render() {
+    return (
+      <div>
+        <h2>Certifications</h2>
+      </div>
+    );
+  }
+}
---
+++ src/details/content/education.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Education extends React.Component {
+    render() {
+    return (
+      <div>
+        <h2>Education</h2>
+      </div>
+    );
+  }
+}
