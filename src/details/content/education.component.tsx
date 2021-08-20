c3bd9dd373ec24f74d362a3275b40b792126f473
Debraj Kundu <kundudebraj4272@gmail.com>
2021-08-20 18:28:31 +0530

    Added my Info In Sections : 1) education 2) home 3) interests 4) skills

--- src/details/content/education.component.tsx
+++ src/details/content/education.component.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 export class Education extends React.Component {
-    render() {
+  render() {
     return (
       <div>
         <h2>Education</h2>
+        Academics: Year Degree/ Examination Board/ University School/ College/
+        Institute Specialization/ Subjects Marks/ CGPA 2020 B. Tech MAKAUT
+        (Maulana Abul Kalam Azad University of Technology) Birbhum Institute of
+        Engineering and Technology, Suri (BIET, Suri) Computer Science and
+        Engineering (CSE) 8.32 DGPA 2016 AISSCE (Sr. Secondary) CBSE Sainik
+        School Purulia ENG, PHY, CHEM, MATH, CS(C++) 84.0 % 2014 AISCE
+        (Secondary) CBSE Sainik School Purulia 9.2 CGPA (87.4 %)
       </div>
     );
   }
--- src/details/content/skills.component.tsx
+++ src/details/content/skills.component.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 export class Skills extends React.Component {
-    render() {
+  render() {
     return (
       <div>
         <h2>Skills</h2>
+        Key skills: 1. Good Organizing Skills. 2. Team work &amp; team
+        management. 3. Good communication skills. Languages known: Language
+        Reading/ Writing Listening/ Speaking English Fluent Fluent Bengali
+        Intermediate Native Fluent Hindi Basic Fluent Technical Skills: Computer
+        Languages Known: 1. C / C++ 2. Java 3. Python 4. PHP, HTML, CSS, JS 5.
+        Visual Basic Computer Skills/ Technologies Known: 1. Git/ Github 2.
+        Docker 3. Machine Learning 4. Web Development 5. DNS/ Domain Management
+        6. Linux 7. Anaconda/ Jupyter Notebook
       </div>
     );
   }
