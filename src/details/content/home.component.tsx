e2ded025312b2cc2ab78eb8e7feff17578c4175e
Debraj Kundu <kundudebraj4272@gmail.com>
2021-08-20 18:28:31 +0530

    Added my Info In Sections : 1) education 2) home 3) interests 4) skills

--- src/details/content/home.component.tsx
+++ src/details/content/home.component.tsx
@@ -1,10 +1,25 @@
+import { Avatar, Grid } from "@material-ui/core";
 import React from "react";
+import "./home.sass";
 
 export class Home extends React.Component {
-    render() {
+  render() {
     return (
       <div>
         <h2>Home</h2>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          <Grid item xs={6}>
+          <Avatar src="profile-dave.jpg" className="avatar-large" />
+          </Grid>
+          <Grid item xs={6}>
+            <h3>Helllloooo</h3>
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- src/details/content/interests.component.tsx
+++ src/details/content/interests.component.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export class Interests extends React.Component {
-    render() {
+  render() {
     return (
       <div>
         <h2>Interests</h2>
+        Hobbies &amp; Interests: 1. Learning New Things 2. Reading Novels 3.
+        Sketching 4. Listening music
       </div>
     );
   }
