698ffce808c7d0deae075c33dc5eb434c7cb8310
Debraj Kundu <kundudebraj4272@gmail.com>
2021-08-22 18:54:28 +0530

    Improving skill page

--- src/details/content/skilllist.fa.tsx
+++ src/details/content/skilllist.fa.tsx
@@ -2,207 +2,207 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export const faskills = [
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "git-alt"],
-      }),
-      label: "Git",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "github"],
-      }),
-      label: "GitHub",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "gitlab"],
-      }),
-      label: "GitLab",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "bitbucket"],
-      }),
-      label: "Bitbucket",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "html5"],
-      }),
-      label: "HTML 5",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "css3"],
-      }),
-      label: "CSS 3",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "js"],
-      }),
-      label: "JavaScript",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "php"],
-      }),
-      label: "php",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "bootstrap"],
-      }),
-      label: "Bootstrap",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "angular"],
-      }),
-      label: "Angular",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "react"],
-      }),
-      label: "React",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "docker"],
-      }),
-      label: "Docker",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "java"],
-      }),
-      label: "Java",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "python"],
-      }),
-      label: "Python",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "sass"],
-      }),
-      label: "Sass",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "markdown"],
-      }),
-      label: "Markdown",
-    },
-    {
-      include: false,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "node-js"],
-      }),
-      label: "Node.js",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "npm"],
-      }),
-      label: "npm",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "yarn"],
-      }),
-      label: "Yarn",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "wordpress"],
-      }),
-      label: "WordPress",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "sourcetree"],
-      }),
-      label: "Sourcetree",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "cpanel"],
-      }),
-      label: "cPanel",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "confluence"],
-      }),
-      label: "Confluence",
-    },
-    {
-      include: true,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "cloudflare"],
-      }),
-      label: "Cloudflare",
-    },
-    {
-      include: false,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "android"],
-      }),
-      label: "Android",
-    },
-    {
-      include: false,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "aws"],
-      }),
-      label: "Amazon Web Services",
-    },
-    {
-      include: false,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "codepen"],
-      }),
-      label: "CodePen",
-    },
-    {
-      include: false,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "jenkins"],
-      }),
-      label: "Jenkins",
-    },
-    {
-      include: false,
-      faicon: React.createElement(FontAwesomeIcon,{
-        icon: ["fab", "hackerrank"],
-      }),
-      label: "HackerRank",
-    },
-  ];
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "git-alt"],
+    }),
+    label: "Git",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "github"],
+    }),
+    label: "GitHub",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "gitlab"],
+    }),
+    label: "GitLab",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "bitbucket"],
+    }),
+    label: "Bitbucket",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "html5"],
+    }),
+    label: "HTML 5",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "css3"],
+    }),
+    label: "CSS 3",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "js"],
+    }),
+    label: "JavaScript",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "php"],
+    }),
+    label: "php",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "bootstrap"],
+    }),
+    label: "Bootstrap",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "angular"],
+    }),
+    label: "Angular",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "react"],
+    }),
+    label: "React",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "docker"],
+    }),
+    label: "Docker",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "java"],
+    }),
+    label: "Java",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "python"],
+    }),
+    label: "Python",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "sass"],
+    }),
+    label: "Sass",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "markdown"],
+    }),
+    label: "Markdown",
+  },
+  {
+    include: false,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "node-js"],
+    }),
+    label: "Node.js",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "npm"],
+    }),
+    label: "npm",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "yarn"],
+    }),
+    label: "Yarn",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "wordpress"],
+    }),
+    label: "WordPress",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "sourcetree"],
+    }),
+    label: "Sourcetree",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "cpanel"],
+    }),
+    label: "cPanel",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "confluence"],
+    }),
+    label: "Confluence",
+  },
+  {
+    include: true,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "cloudflare"],
+    }),
+    label: "Cloudflare",
+  },
+  {
+    include: false,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "android"],
+    }),
+    label: "Android",
+  },
+  {
+    include: false,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "aws"],
+    }),
+    label: "Amazon Web Services",
+  },
+  {
+    include: false,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "codepen"],
+    }),
+    label: "CodePen",
+  },
+  {
+    include: false,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "jenkins"],
+    }),
+    label: "Jenkins",
+  },
+  {
+    include: false,
+    faicon: React.createElement(FontAwesomeIcon, {
+      icon: ["fab", "hackerrank"],
+    }),
+    label: "HackerRank",
+  },
+];
