d2b35a4c9a8fc2e080201ab7110c46c43bf74eb4
Debraj Kundu <kundudebraj4272@gmail.com>
2021-08-30 22:25:45 +0530

    Added Masonry to Certificates

--- src/details/content/certificates.tsx
+++ src/details/content/certificates.tsx
@@ -2,7 +2,17 @@ import { Typography } from "@material-ui/core";
 import React from "react";
 import { certCards } from "./items/cert.card";
 import "./certificates.sass";
+import Masonry from "masonry-layout";
+
 export class Certs extends React.Component {
+  componentDidMount(){
+    var msnry = new Masonry( '.creds-container', {
+      columnWidth: 520,
+      itemSelector: ".certificate",
+      gutter: 16,
+      horizontalOrder: true,
+    });
+  }
   render() {
     return (
       <>
