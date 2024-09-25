6d386c4fd11a379829e768afc259944e306b098d
Debraj Kundu <kundudebraj4272@gmail.com>
2024-09-25 07:15:56 +0530

    Update Imlementation

--- src/details/content/experience.tsx
+++ src/details/content/experience.tsx
@@ -2,6 +2,7 @@ import { Timeline } from "@mui/lab";
 import {
   Chip,
   Container,
+  Grid,
   List,
   ListItem,
   ListItemIcon,
@@ -11,23 +12,41 @@ import {
 import { AxiosResponse } from "axios";
 import { Buffer } from "buffer";
 import { DateTime } from "luxon";
-import React, { Suspense, useCallback, useEffect, useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  Suspense,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { CgSpinner } from "react-icons/cg";
 import { GoDotFill } from "react-icons/go";
 import { AxiosClient } from "../../HTTPClient";
+import { durationAsString } from "../../utils/date-time";
 import "./experience.sass";
 import { IWorkItem, WorkItem } from "./items/data/types/WorkItem";
 import { WorkCard } from "./items/work.card";
 
 export const Experience = () => {
+  const [expDuration, setExpDuration] = useState<string>("");
   return (
     <>
-      <Typography variant="h2" gutterBottom className="extra-padded title">
-        Experience
-      </Typography>
+      <Grid container direction="row" alignItems="baseline" wrap="nowrap">
+        <Grid item xs="auto" sm="auto" md="auto" lg="auto" xl="auto">
+          <Typography variant="h2" gutterBottom className="extra-padded title">
+            Experience
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <Typography variant="h5" gutterBottom className="extra-padded" component="h2">
+            {expDuration ? "(" + expDuration + ")" : ""}
+          </Typography>
+        </Grid>
+      </Grid>
       <Container className="root-content experience-container">
         <Suspense fallback={<CgSpinner />}>
-          <ExperienceBoard />
+          <ExperienceBoard setTotalExperience={setExpDuration} />
         </Suspense>
       </Container>
     </>
@@ -67,7 +86,7 @@ const ExperienceSkill = (props: { skills: string[] }) => {
   const [expskills, setExpskills] = useState<React.ReactElement[]>([]);
   const convertToList = useCallback(() => {
     setExpskills([]);
-    props.skills.reverse().forEach((val) => {
+    props.skills.toReversed().forEach((val) => {
       const key = Buffer.from(val, "binary").toString("base64");
       setExpskills((expskills) => [
         <Chip style={{ margin: "5px" }} label={val} key={key} />,
@@ -83,13 +102,20 @@ const ExperienceSkill = (props: { skills: string[] }) => {
   return <>{expskills}</>;
 };
 
-const ExperienceBoard = () => {
+const ExperienceBoard = (props: {
+  setTotalExperience: Dispatch<SetStateAction<string>>;
+}) => {
   const [exp, setExp] = useState<React.ReactElement[]>([]);
 
-  const handleResponse = (res: AxiosResponse) => {
+  const handleResponse = (
+    res: AxiosResponse<IWorkItem[]>,
+    setTotalExperience: Dispatch<SetStateAction<string>>
+  ) => {
     setExp([]);
     const length = res.data.length;
-    res.data.reverse().forEach((element: IWorkItem, index: number) => {
+    let start: DateTime | undefined = undefined;
+    let end: DateTime | undefined = undefined;
+    res.data.reverse().forEach((element, index) => {
       let key: string =
         element.designation + element.organization + element.from;
       key = Buffer.from(key, "binary").toString("base64");
@@ -103,17 +129,39 @@ const ExperienceBoard = () => {
         skills: <ExperienceSkill skills={element.skills} />,
       };
       if (element.include) {
+        if (!start) {
+          start = value.from;
+        } else {
+          start = DateTime.min(start, value.from);
+        }
+        if (!end && value.to) {
+          end = value.to;
+        } else if (end) {
+          if (!value.to) {
+            end = DateTime.now();
+          } else {
+            end = DateTime.max(end, value.to);
+          }
+        }
         setExp((exp) => [
           <WorkCard index={index} value={value} key={key} length={length} />,
           ...exp,
         ]);
       }
     });
+    setTotalExperience(
+      durationAsString(
+        !start ? DateTime.now() : start,
+        !end ? DateTime.now() : end
+      )
+    );
   };
 
   const dataFetch = useCallback(() => {
-    AxiosClient().get("work.list.json").then(handleResponse);
-  }, []);
+    AxiosClient()
+      .get("work.list.json")
+      .then((res) => handleResponse(res, props.setTotalExperience));
+  }, [props.setTotalExperience]);
 
   useEffect(() => {
     dataFetch();
---
+++ src/utils/date-time.ts
@@ -0,0 +1,30 @@
+import { DateTime, Duration } from "luxon";
+
+export const durationAsString = (from: DateTime, to?: DateTime): string => {
+  let duration: Duration;
+  let formatedString = "";
+  if (!to) {
+    duration = DateTime.now().diff(from, ["year", "months"], {
+      conversionAccuracy: "longterm",
+    });
+  } else {
+    duration = to.diff(from, ["years", "months"], {
+      conversionAccuracy: "longterm",
+    });
+  }
+
+  if (parseInt(duration.years.toFixed(0)) !== 0) {
+    formatedString += duration.years.toFixed(0) + " yr";
+    formatedString += Math.abs(duration.years) > 1 ? "s" : "";
+  }
+
+  if (parseInt(duration.months.toFixed(0)) !== 0) {
+    if (formatedString.length > 0) {
+      formatedString += " "
+    }
+    formatedString += Math.ceil(Number(duration.months.toFixed(2))) + " mth";
+    formatedString += Math.abs(duration.months) > 1 ? "s" : "";
+  }
+
+  return formatedString;
+};
